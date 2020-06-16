d706683b793dc0f9c1b2f6edb39b3d26a658627f
chen <chenxiaoguanghao@163.com>
2020-06-16 12:52:24 +0800

    ioc

--- src/main/java/com/chen/demo/config/IocTest.java
+++ src/main/java/com/chen/demo/config/IocTest.java
@@ -4,18 +4,15 @@ package com.chen.demo.config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.chen.demo.pojo.User;
-
 
 public class IocTest {
-	
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
 		User user = ctx.getBean(User.class);
 		
 		System.out.println(user.getId());
-		
+		System.out.println(user.getNote());
 
 	}
 
