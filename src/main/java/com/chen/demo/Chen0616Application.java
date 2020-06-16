224169f80433130328bbd636ea69b08d2c106474
chen <chenxiaoguanghao@163.com>
2020-06-16 11:18:52 +0800

    Initial commit

---
+++ src/main/java/com/chen/demo/Chen0616Application.java
@@ -0,0 +1,13 @@
+package com.chen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chen0616Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chen0616Application.class, args);
+	}
+
+}
